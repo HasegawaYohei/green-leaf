5fb511cd413672b004621c4eb813b36e86959ce8
RyosukeTakahashi <ramuniku@gmail.com>
2018-03-28 14:44:33 +0900

    Add news section

--- clientTheme/home.php
+++ clientTheme/home.php
@@ -12,7 +12,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="full-width-page-wrapper">
-  
+
+  <main>
   <section id="work_domain">
     <h2>事業紹介</h2>
   </section>
@@ -22,17 +23,16 @@ $container = get_theme_mod( 'understrap_container_type' );
   <section id="news">
     <h2>NEWS</h2>
     <div class='d-flex'>
-        <div class="col-2p-2">2018-03-26</div>
-
-
-
+        <div class="col-2 p-2">2018-03-26</div>
+        <div class="col-1 p-2">ニュース</div>
+        <div class="col-8 p-2">Web pageをリニューアルしました</div>
+        <div class="col-1 p-2">icon</div>
     </div>
-
   </section>
-
+  </main>
   <div class="<?php echo esc_attr( $container ); ?>" id="content">
-  
- 
+
+
 		<div class="row">
 
 			<div class="col-md-12 content-area" id="primary">
