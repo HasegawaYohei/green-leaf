8e7a4829295daca99c99ba283014045cb8f29734
Hasegawa Yohei <yoheybrains@gmail.com>
2019-03-17 22:35:23 +0900

    modified: remove unnecessary lines

--- clientTheme/loop-templates/content-page.php
+++ clientTheme/loop-templates/content-page.php
@@ -5,5 +5,9 @@
  * @package understrap
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 ?>
+<div class="to-top" id="toTop"><span><i class="fas fa-angle-right"></i></span></div>
 <?php the_content(); ?>
--- clientTheme/single.php
+++ clientTheme/single.php
@@ -6,46 +6,26 @@
  */
 
 get_header();
-$container   = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="single-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
+	<div class="container-flud">
 		<div class="row">
-
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
-			<main class="site-main" id="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-
-						<?php understrap_post_nav(); ?>
-
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-		</div><!-- #primary -->
-
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-	</div><!-- .row -->
-
-</div><!-- Container end -->
-
+			<div class="col-12 mx-auto section-box">
+				<div class="row section-row">
+					<div class="col-11 col-sm-6 mx-auto">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'loop-templates/content', 'event' ); ?>
+						<?php endwhile; // end of the loop. ?>
+					</div>
+					<div class="col-11 col-sm-5 mx-auto">
+						<?php get_template_part( 'global-templates/event-sidebar' ); ?>
+					</div>
+				</div>
+			</div>
+		</div><!-- .row -->
+	</div><!-- Container end -->
 </div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
