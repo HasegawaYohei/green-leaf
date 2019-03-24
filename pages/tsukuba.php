c9b72796d54515fec03f5894bc22e4d2eaa836a6
Hasegawa Yohei <yoheybrains@gmail.com>
2019-03-25 06:12:02 +0900

    added: tsukuba page

---
+++ pages/tsukuba.php
@@ -0,0 +1,211 @@
+<!-- Key Visual -->
+<div class="container-flud pb-5 bg-white">
+  <div class="row">
+    <div class="col-12 mx-auto section-box">
+      <div class="row section-row text-center">
+        <div class="col-12 text-center mb-5">
+          <span class="btn-group">
+            <a class="btn btn-green-sm" href="https://green-leaf.info/cafe/tsukuba/">つくば市</a>
+            <a class="btn btn-custom outline-green color-green" href="https://green-leaf.info/cafe/hitachi/">日立市</a>
+          </span>
+        </div>
+        <div class="col-11 col-sm-12">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-12 mt-3">
+          <p class="section-title-noline my-2">ハーブかふぇROOT</p>
+          <p class="text-sm">
+            〒305-0015 茨城県つくば市二ノ宮4-1-26<br>
+            TEL&FAX  029-893-4694<br>
+            OPEN ／月 10:00～13:00、水・木・金・土・日 10:00～18:00<br>
+            （LUNCH 11:00～15:00）<br>
+            CLOSE／火<br>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- LUNCH MENU -->
+<div class="container-flud py-5 bg-green">
+  <div class="row">
+    <div class="col-12 mx-auto section-box">
+      <p class="text-center color-green section-title mb-4 mb-lg-5">LUNCH MENU</p>
+      <div class="row section-row">
+        <div class="col-11 col-sm-5">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 shop-info-box">
+          <p class="section-title-noline mb-2">レディース・ビューティー・ランチ A（限定10食）<p>
+          <span class="text-bold">1,400円 (税別)</span>
+          <ul>
+            <li>前菜:</li>
+            <li>主菜:</li>
+            <li>副菜:</li>
+            <li>オリジナルハーブティー（ご要望に応じてその場でブレンドいたします）</li>
+          </ul>
+          <p class="color-red text-sm">
+            ※食材がなくなり次第、売切れとさせていただきますので、ご予約いただくことをお勧めします（<a class="color-green" jclass="color-green" href="">ご予約フォームはこちら</a>）<br>
+            ※写真はイメージです。実際とは異なる場合がございます
+          </p>
+        </div>
+      </div>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5 order-1 order-sm-2">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 order-2 order-sm-1 shop-info-box">
+          <p class="section-title-noline mb-2">レディース・ビューティー・ランチ B（限定10食）<p>
+          <span class="text-bold">1,600円 (税別)</span>
+          <ul>
+            <li>前菜:</li>
+            <li>主菜:</li>
+            <li>副菜:</li>
+            <li>オリジナルハーブティー（ご要望に応じてその場でブレンドいたします）</li>
+          </ul>
+          <p class="color-red text-sm">
+            ※食材がなくなり次第、売切れとさせていただきますので、ご予約いただくことをお勧めします（<a class="color-green" href="">ご予約フォームはこちら</a>）<br>
+            ※写真はイメージです。実際とは異なる場合がございます
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- TEA & SWEETS MENU -->
+<div class="container-flud py-5 bg-white">
+  <div class="row">
+    <div class="col-12 mx-auto section-box">
+      <p class="text-center color-orange section-title mb-4 mb-lg-5">TEA & SWEETS MENU</p>
+      <div class="row section-row">
+        <div class="col-11 col-sm-5">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 shop-info-box">
+          <p class="section-title-noline mb-2">オリジナルブレンドハーブティー<p>
+          <span class="text-bold">800円 (税別)</span>
+          <ul>
+            <li>お客様のお悩みやご要望をカウンセリングさせていただいた上で、お客様にぴったりのハーブテイーをブレンドいたします</li>
+            <li>ハーブティーはおよそ3回に分けて淹れて楽しむことができます</li>
+          </ul>
+          <p class="color-red text-sm">
+            ※事前に外来質問票フォームに記入いただくとより効果的なカウンセリングが可能になります（<a class="color-green" href="">外来質問票フォームフォームはこちら</a>）<br>
+            ※写真はイメージです。実際とは異なる場合がございます
+          </p>
+        </div>
+      </div>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5 order-1 order-sm-2">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 order-2 order-sm-1 shop-info-box">
+          <p class="section-title-noline mb-2">オリジナルブレンドハーブティー + スイーツセット<p>
+          <span class="text-bold">1,200円 (税別)</span>
+          <ul>
+            <li>お客様のお悩みやご要望をカウンセリングさせていただいた上で、お客様にぴったりのハーブテイーをブレンドいたします</li>
+            <li>ハーブティーはおよそ3回に分けて淹れて楽しむことができます</li>
+          </ul>
+          <p class="color-red text-sm">
+            ※事前に外来質問票フォームに記入いただくとより効果的なカウンセリングが可能になります（<a class="color-green" href="">外来質問票フォームフォームはこちら</a>）<br>
+            ※写真はイメージです。実際とは異なる場合がございます
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- STAFF -->
+<div class="container-flud py-5 bg-green">
+  <div class="row">
+    <div class="col-12 mx-auto section-box">
+      <p class="text-center color-green section-title mb-4 mb-lg-5">STAFF</p>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/osone_michiru.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 shop-info-box">
+          <p class="section-title-noline mb-2">おおそね みちる<p>
+          <span class="text-bold">オーナー / プロデューサー</span>
+          <p>
+          「このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。」
+          </p>
+        </div>
+      </div>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5 order-1 order-sm-2">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/osone_michiru.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 order-2 order-sm-1 shop-info-box">
+          <p class="section-title-noline mb-2">おおそね みちる<p>
+          <span class="text-bold">オーナー / プロデューサー</span>
+          <p>
+          「このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。」
+          </p>
+        </div>
+      </div>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/osone_michiru.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 shop-info-box">
+          <p class="section-title-noline mb-2">おおそね みちる<p>
+          <span class="text-bold">オーナー / プロデューサー</span>
+          <p>
+          「このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。」
+          </p>
+        </div>
+      </div>
+      <div class="row section-row mt-6">
+        <div class="col-11 col-sm-5 order-1 order-sm-2">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/osone_michiru.png" class="img-fluid">
+        </div>
+        <div class="col-11 col-sm-7 order-2 order-sm-1 shop-info-box">
+          <p class="section-title-noline mb-2">おおそね みちる<p>
+          <span class="text-bold">オーナー / プロデューサー</span>
+          <p>
+          「このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。このお店で実現したいことを160文字くらいで。」
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- MAP -->
+<div class="container-flud py-5 bg-white">
+  <div class="row">
+    <div class="col-12 mx-auto section-box">
+      <div class="row section-row">
+        <div class="col-11 col-sm-12 mt-3">
+          <div class="map-box">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3225.313830761192!2d140.11302361507776!3d36.061448716633244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220cbc35cffb79%3A0xd43cdeed6f06f4e7!2z44OP44O844OW44Kr44OV44KnIFJPT1Q!5e0!3m2!1sja!2sjp!4v1553753846984" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="row section-row mt-3">
+        <div class="col-11 col-sm-6">
+          <p class="section-title-noline">ハーブカフェROOT</p>
+          <p class="text-sm">
+            〒305-0015 茨城県つくば市二ノ宮4-1-26<br>
+            TEL&FAX  029-893-4694<br>
+            OPEN ／月 10:00～13:00、水・木・金・土・日 10:00～18:00<br>
+            （LUNCH 11:00～15:00）<br>
+            CLOSE／火<br><br>
+            ●車でお越しの方<br><br>
+            ●電車+バスでお越しの方<br>
+          </p>
+          <a href="https://goo.gl/maps/qgrpWsfDxRA2" target="_blank" class="btn btn-medium outline-orange color-orange mt-5">Google Maps &nbsp;<i class="fas fa-angle-right"></i></a>
+        </div>
+        <div class="col-11 col-sm-6">
+          <img src="https://green-leaf.info/wp-content/uploads/2019/01/herb_cafe_root.png" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
