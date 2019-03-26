797419d1d44dbb3ba652f434866f0c64eed18596
Hasegawa Yohei <yoheybrains@gmail.com>
2019-03-27 08:25:10 +0900

    modified: header.php

--- clientTheme/header.php
+++ clientTheme/header.php
@@ -4,7 +4,7 @@
  *
  * Displays all of the <head> section and everything up till <div id="content">
  *
- * @package understrap
+ * @package GREEN LEAF
  */
 
 $container = get_theme_mod( 'understrap_container_type' );
@@ -44,7 +44,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <body <?php body_class(); ?>>
   <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand mx-0" href=""><img src="https://green-leaf.info/wp-content/uploads/2019/01/top_left_logo.png"></a>
+    <a class="navbar-brand mx-0" href="https://green-leaf.info/"><img src="https://green-leaf.info/wp-content/uploads/2019/01/top_left_logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -52,26 +52,79 @@ $container = get_theme_mod( 'understrap_container_type' );
       <ul class="navbar-nav mr-auto">
       </ul>
       <ul class="navbar-nav">
-				<li class="nav-item">
+				<li class="nav-item d-inline d-lg-none text-center">
+					<a class="nav-link text-bold color-green" href="https://green-leaf.info/">ホーム</a>
+				</li>
+				<li class="nav-item text-center">
 					<a class="nav-link text-bold color-green" href="https://green-leaf.info/concept/">コンセプト</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link text-bold color-green" href="https://green-leaf.info/cace/tsukuba/">店舗案内</a>
+				<li class="nav-item d-inline d-lg-none text-center">
+					<a class="nav-link text-bold color-green" href="https://green-leaf.info/tea/">ハーブかふぇROOT（つくば市）</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item d-inline d-lg-none text-center">
+					<a class="nav-link text-bold color-green" href="https://green-leaf.info/tea/">ハーブ専門店グリーンリーフ（日立市）</a>
+				</li>
+				<li class="nav-item d-none d-lg-inline">
+					<div class="dropdown drop-hover">
+						<a class="dropdown-toggle nav-link text-bold color-green" href="https://green-leaf.info/cafe/tsukuba/" data-toggle="dropdown">
+							店舗案内
+						</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/cafe/tsukuba/">つくば市</a>
+							</li>
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/cafe/hitachi/">日立市</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li class="nav-item text-center">
 					<a class="nav-link text-bold color-green" href="https://green-leaf.info/tea/">商品紹介</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link text-bold color-green" href="https://green-leaf.info/">イベント</a>
+				<li class="nav-item text-center">
+					<a class="nav-link text-bold color-green" href="https://green-leaf.info/event/">イベント</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item d-inline d-lg-none text-center">
 					<a class="nav-link text-bold color-green" href="https://green-leaf.info/course/">プログラム紹介</a>
 				</li>
-        <li class="nav-item">
-        <li class="nav-item">
+				<li class="nav-item d-none d-lg-inline">
+					<div class="dropdown drop-hover">
+						<a class="dropdown-toggle nav-link text-bold color-green" href="https://green-leaf.info/course/" data-toggle="dropdown">
+							プログラム紹介
+						</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/course/herbal-therapist/">ハーバルセラピストコース</a>
+							</li>
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/course/">日本のハーブセラピストコース</a>
+							</li>
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/course/">シニアハーバルセラピストコース</a>
+							</li>
+							<li>
+								<a class="dropdown-item color-white" href="https://green-leaf.info/course/">ハーバルプラクティショナーコース</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li class="nav-item d-inline d-lg-none mt-3 text-sm text-center">
+					<a class="nav-link text-bold color-green" href="https://green-leaf.info/contact/" style="display:inline">お問い合わせ</a>｜<a class="nav-link text-bold color-green" href="https://green-leaf.info/privacy-policy/" style="display:inline">プライバシーポリシー</a>
+				</li>
+				<li class="nav-item d-inline d-lg-none mt-3 text-center">
+					<a href="https://ameblo.jp/michiru-herbgarden/" target="_blank"><img src="https://green-leaf.info/wp-content/uploads/2019/01/blog_banner_link.png" class="img-fluid"></a>
+				</li>
+				<li class="nav-item d-inline d-lg-none mt-3 text-center">
+					<a href="https://green-leaf.shop-pro.jp/" target="_blank"><img src="https://green-leaf.info/wp-content/uploads/2019/01/onlineshop_banner.png" class="img-fluid"></a>
+				</li>
+				<li class="nav-item d-inline d-lg-none mt-3 text-center">
+					<a href="https://green-leaf.info/counselling/" target="_blank"><img src="https://green-leaf.info/wp-content/uploads/2019/03/counceling-banner.png" class="img-fluid"></a>
+				</li>
+        <li class="nav-item d-none d-lg-inline">
           <a class="nav-link text-bold color-green" href="https://ameblo.jp/michiru-herbgarden/" target="_blank">ブログ<img src="https://green-leaf.info/wp-content/uploads/2019/03/blog-logo.png"></a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item d-none d-lg-inline">
           <a class="nav-link btn btn-green color-white" style="width: 180px !important; margin: 0 !important;" href="https://green-leaf.shop-pro.jp/" target="_blank">オンラインショップ<img src="https://green-leaf.info/wp-content/uploads/2019/03/shop-logo-white.png"></a>
         </li>
       </ul>
